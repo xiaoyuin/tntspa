85b051efb1e1dc7491bda4bf1ff98aab3e305056
Yin Xiaoyu <yinxiaoyu39999@gmail.com>
2018-08-16 12:34:29 +0200

    Added gensim usage in run_hpc

--- run_hpc.py
+++ run_hpc.py
@@ -1,12 +1,13 @@
 import utils
 from model import MyModel
-from fasttext_vector import FastTextVector
-
+# from fasttext_vector import FastTextVector
+from gensim_vector import GensimFastTextVector
 
 input_texts = utils.read_lines("data/monument_600/train.en")
 target_texts = utils.read_lines("data/monument_600/train.sparql")
 
-fasttext_model = FastTextVector("data/wiki.en.bin")
+# fasttext_model = FastTextVector("data/wiki.en.bin")
+fasttext_model = GensimFastTextVector("data/wiki.en.bin")
 model = MyModel(input_word_vector=fasttext_model, num_layers=2)
 model.train(input_texts, target_texts, epochs=30)
 
